9905788f6d78cdae9a37822ea40ce22b8c4b19ab
ahmed bendary <bendarya58@gmail.com>
2026-02-18 00:45:51 +0200

    auth

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,7 +6,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ 
   storage: storage,
   limits: {
-    fileSize: 4 * 1024 * 1024, // 4MB to stay under Vercel's limit
+    fileSize: 4 * 1024 * 1024, 
   }
 });
 
