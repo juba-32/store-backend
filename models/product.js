6e90839f5fea6636a29edd3b7037ab28482ab80f
ahmed bendary <bendarya58@gmail.com>
2026-06-01 14:53:05 +0300

    updated product shcema

--- models/product.js
+++ models/product.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  image: String,
-  title: String,
-  category: String,
-  price: Number,
-  discount: Number,
-  color: String,
-  description: String,
-  inStock: Boolean,
-  brand: String,
-  model: String
-});
+  title: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String },
+  discount: { type: Number, default: 0 },
+  color: { type: String },
+  inStock: { type: Boolean, default: true },
+  brand: { type: String },
+  model: { type: String },
+  image: { type: String },
+  images: { type: [String], default: [] }
+}, { timestamps: true });
 
 const Product = mongoose.model("Product", productSchema);
 
